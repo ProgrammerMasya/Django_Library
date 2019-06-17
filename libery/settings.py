ddfd0b9e2347ecf71e7d88226c864244b1dd742a
Vladislav <vlad3052001@gmail.com>
2019-06-17 16:00:18 +0300

    changed bd to postgres

--- libery/settings.py
+++ libery/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'libery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'libery_db',
+        'USER': 'masya',
+        'PASSWORD': 'rb,thnhjy2001',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
